21d2664411781bf2ace9bad5a20a2fb0a8a71c34
JestonBlu <jestonblu@gmail.com>
2018-04-22 08:32:58 -0500

    update

--- demo.py
+++ demo.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from bokeh.plotting import figure, show
-from bokeh.io import output_notebook
+from bokeh.io import output_notebook, output_file
 
 dta = pd.read_csv("~/Projects/pi-cluster/data/rpi_stats.csv")
 
-output_notebook()
+
 
 p = figure(plot_width = 800, plot_height = 400)
-p.line(x=dta.index, y=dta.rpi1_cpu_tmp)
+p.line(x=dta.index, y=dta.rpi1_cpu_tmp, line_alpha=.3)
+p.circle(x=dta.index, y=dta.rpi1_cpu_tmp, alpha=.3)
+
+output_file('index.html')
 show(p)
