4184f21ad4dad894f5b79d5c32307c78deaaeaea
jestonblu <jestonblu@gmail.com>
2018-05-03 20:22:32 -0500

    update

--- demo.py
+++ demo.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from bokeh.plotting import figure, show
-from bokeh.io import output_notebook, output_file
+from bokeh.io import output_file
 
 dta = pd.read_csv("~/Projects/pi-cluster/data/rpi_stats.csv")
-
-
 
 p = figure(plot_width = 800, plot_height = 400)
 p.line(x=dta.index, y=dta.rpi1_cpu_tmp, line_alpha=.3)
